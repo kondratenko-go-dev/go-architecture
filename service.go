9dac77ceb18d08edb0a6b6186fce67d378067acc
kondratenko-go-dev <kondratenko.aleksandr.go.dev@gmail.com>
2025-11-23 15:30:44 +0200

    Extend PersonStorage with FindByName and Update, add service methods

--- service.go
+++ service.go
@@ -8,7 +8,12 @@ func NewPersonService(storage PersonStorage) *PersonService {
 	return &PersonService{storage}
 }
 
-func (s *PersonService) CreatePerson(name string, age int, city, street string) (*Person, error) {
+func (s *PersonService) CreatePerson(
+	name string,
+	age int,
+	city, street string,
+) (*Person, error) {
+
 	person, err := NewPerson(name, age, city, street)
 	if err != nil {
 		return nil, err
@@ -17,6 +22,32 @@ func (s *PersonService) CreatePerson(name string, age int, city, street string) 
 		return nil, err
 	}
 	return person, nil
+}
+
+func (s *PersonService) UpdatePerson(
+	id int64,
+	name string,
+	age int,
+	city, street string,
+) (*Person, error) {
+
+	existing, err := s.storage.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	updated, err := NewPerson(name, age, city, street)
+	if err != nil {
+		return nil, err
+	}
+
+	updated.id = existing.id
+
+	if err := s.storage.Update(updated); err != nil {
+		return nil, err
+	}
+
+	return updated, nil
 }
 
 func (s *PersonService) ListPeople() ([]*Person, error) {
@@ -29,6 +60,10 @@ func (s *PersonService) GetPerson(id int64) (*Person, error) {
 
 func (s *PersonService) DeletePerson(id int64) error {
 	return s.storage.Delete(id)
+}
+
+func (s *PersonService) FindByName(name string) ([]*Person, error) {
+	return s.storage.FindByName(name)
 }
 
 func PrintGreeting(greeter []Greeter) {
